361fc23bf0a98f8bc4c5fdc254bf425a0d90cee1
gsmohamedfarooq <gsmohamedfarooq@gmail.com>
2026-09-15 16:13:25 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -7,7 +7,7 @@ def test_home():
     response = client.get("/")
 
     assert response.status_code == 200
-    assert response.data.decode() == "Hello from DevOps Platform"
+    assert response.data.decode() == "Hello from DevOps Platform - V2"
 
 
 def test_health():
